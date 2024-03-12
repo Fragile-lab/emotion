9e605f50c5468d114dafafe66ea73f55b81f1065
fragile-lab <fragile.lab@gmail.com>
2024-03-12 21:53:09 +0400

    frame4ready

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 $(document).ready(function () {
-    $('.header__burger').click(function(e) {
+    $('.header__burger').click(function (e) {
         $('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
-    $('.frame3__button').hover(function(e) {
+    $('.frame3__button').hover(function (e) {
         $('.frame3__button').addClass('open');
-    })
+    });
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        rows: 2,
+        adaptiveHeight: true,
+        infinite: true,
+        slidesToShow: 2,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        speed: 1000,
+        easing: 'ease-in-out',
+        pauseOnFocus: true,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+        responsive: [
+            {
+                breakpoint: 769, settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
 });
