3d2ee1526484581b8986d93cf4b92580f6e98d66
Fragile Lab <fragile_lab@gmail.com>
2024-04-08 18:04:46 +0400

    fixburger

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {
-    $('.header__burger').click(function (e) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
     $('.frame3__button').hover(function (e) {
         $('.frame3__button').addClass('open');
     });
 });
+//index
