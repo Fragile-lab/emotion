b6779ab0f69906f12c02d42c9d3e072131e1be00
Fragile Lab <fragile_lab@gmail.com>
2024-04-08 21:55:38 +0400

    tango_slider_ready

--- js/tango.js
+++ js/tango.js
@@ -1,17 +1,45 @@
 function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-       change.target.classList.add('element-show');
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
+//sider
+$(document).ready(function () {
+
+  $('.frame3 .slider').slick({
+    arrows: false,
+    dots: false,
+    rows: 1,
+    adaptiveHeight: true,
+    infinite: true,
+    slidesToShow: 4,
+    autoplay: false,
+    speed: 1000,
+    easing: 'ease-in-out',
+    responsive: [
+      {
+        breakpoint: 768, settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 570, settings: {
+          slidesToShow: 1,
+        }
       }
-    });
-  }
-  
-  let options = {
-    threshold: [0.5] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.element-animation');
-  
-  for (let elm of elements) {
-    observer.observe(elm);
-  }
-  //tango
+    ]
+  });
+});
+//tango
